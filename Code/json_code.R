277947ac8ef2d9aa40f378ecae32474ab0ad395c
Denise Bradford <52838109+drbradford12@users.noreply.github.com>
2020-09-16 21:12:31 -0500

    Updating code with RSocrata path

--- Code/json_code.R
+++ Code/json_code.R
@@ -3,44 +3,54 @@ library(jsonlite)
 library(tidyverse)
 library(purrr)
 library(tibble)
+library(RSocrata)
 
-#TO-DO: figure out the way to get more information for the bigger files. (liquor stores and liquor sales)
 #TO-DO: Find the best way to download the child care data that doesn't have a JSON url
-pull_data <- function (x) {
-  data = fromJSON(rawToChar(x$content))
-  data
-  }
-
 
 data_sources <- tibble::tribble(
   ~name, ~url,
-  "assisted living", "https://data.iowa.gov/resource/67aj-bdft.json",
-  "city budget expenditures", "https://data.iowa.gov/resource/jy6h-2e5x.json",
-  "city budget revenue", "https://data.iowa.gov/resource/bzed-t5zc.json",
-  "child abuse occurrences", "https://data.iowa.gov/resource/mh9d-fias.json",
-  "child abuse victims","https://data.iowa.gov/resource/n84y-ufum.json",
-  "child welfare assessments", "https://data.iowa.gov/resource/er5e-kmgq.json", 
-  "family investment program", "https://data.iowa.gov/resource/79c3-mzyc.json",
-  "fire department census", "https://data.iowa.gov/resource/hv43-6ksq.json",
-  "food assistance program statistics", "https://data.iowa.gov/resource/nqiw-f9td.json",
-  "medicaid payments county", "https://data.iowa.gov/resource/jmyd-wk9g.json",
-  "medicaid payments vendor", "https://data.iowa.gov/resource/b3t9-awkp.json",
-  "physical and cultural geographic features", "https://data.iowa.gov/resource/uedc-2fk7.json",
-  "school building directory", "https://data.iowa.gov/resource/spci-5thi.json",
-  "school district revenues" ,"https://data.iowa.gov/resource/pf4i-4nww.json",
-  "unemployment compensation fund status benefits", "https://data.iowa.gov/resource/bbux-m3a4.json",
-  "unemployment insurance benefit payments", "https://data.iowa.gov/resource/aeyn-twxp.json",
-  "liquor_stores", "https://data.iowa.gov/resource/ykb6-ywnd.json",
-  "liquor sales", "https://data.iowa.gov/resource/m3tr-qhgy.json",
-  "quarterly retail sales tax", "https://data.iowa.gov/resource/55fz-vque.json",
-  "registered retirement facilities", "https://data.iowa.gov/resource/cvnj-m3t8.json"
-) %>%
+  "assisted living", "https://data.iowa.gov/api/odata/v4/67aj-bdft",
+  "city budget expenditures", "https://data.iowa.gov/api/odata/v4/jy6h-2e5x",
+  "city budget revenue", "https://data.iowa.gov/api/odata/v4/bzed-t5zc",
+  "child abuse occurrences", "https://data.iowa.gov/api/odata/v4/mh9d-fias",
+  "child abuse victims","https://data.iowa.gov/api/odata/v4/n84y-ufum",
+  "child welfare assessments", "https://data.iowa.gov/api/odata/v4/er5e-kmgq", 
+  "family investment program", "https://data.iowa.gov/api/odata/v4/79c3-mzyc",
+  "fire department census", "https://data.iowa.gov/api/odata/v4/hv43-6ksq",
+  "food assistance program statistics", "https://data.iowa.gov/api/odata/v4/nqiw-f9td",
+  "medicaid payments county", "https://data.iowa.gov/api/odata/v4/jmyd-wk9g",
+  "medicaid payments vendor", "https://data.iowa.gov/api/odata/v4/b3t9-awkp",
+  "physical and cultural geographic features", "https://data.iowa.gov/api/odata/v4/uedc-2fk7",
+  "school building directory", "https://data.iowa.gov/api/odata/v4/spci-5thi",
+  "school district revenues" ,"https://data.iowa.gov/api/odata/v4/pf4i-4nww",
+  "unemployment compensation fund status benefits", "https://data.iowa.gov/api/odata/v4/bbux-m3a4",
+  "unemployment insurance benefit payments", "https://data.iowa.gov/api/odata/v4/aeyn-twxp",
+  "liquor_stores", "https://data.iowa.gov/api/odata/v4/ykb6-ywnd",
+  "liquor sales", "https://data.iowa.gov/api/odata/v4/m3tr-qhgy",
+  "quarterly retail sales tax", "https://data.iowa.gov/api/odata/v4/55fz-vque",
+  "registered retirement facilities", "https://data.iowa.gov/api/odata/v4/cvnj-m3t8"
+) 
+
+#Here is the list of data sources that will take on the data directly from the JSON file
+
+#%>%
   # Download JSON
-  mutate(json = purrr::map(url, GET))
+#  mutate(json = purrr::map(url, read.socrata))
 
 # Get data frames
-data_sources <- data_sources %>%
-  mutate(data = purrr::map(json, pull_data))
+#data_sources <- data_sources %>% 
+#  mutate(data = purrr::map(json, pull_data))
 
 
+#Trying to pull in multiple rows of data via socrata
+#try_json = RSocrata::read.socrata("https://data.iowa.gov/api/odata/v4/m3tr-qhgy")
+
+
+#Export Data to Github Folder
+repo <- repository("~/Documents/Data-Sources") #git@github.com:Shrink-Smart-Data-Science/Data-Sources.git
+pull(repo)
+write_vc(data_sources, file = "Data/name", root = repo, stage = TRUE)
+commit(repo, "Adding new data to repo")
+push(repo)
+read_vc(file = "rel_path/filename", root = repo)
 
