b2349d17b65ff6a8694b2097ffbe7055938bf5b4
Denise Bradford <52838109+drbradford12@users.noreply.github.com>
2020-09-27 16:25:15 -0500

    Code update

--- Code/html_code.R
+++ Code/html_code.R
@@ -1,19 +1,16 @@
 library(rvest)
 library(RSelenium)
 library(htmltab)
-
 library(splashr)
 
+#Using Splash
 install_splash()    # run this once to install the docker image
 sp <- start_splash()
-
 pg <- render_html(url = 'https://dhs.iowa.gov/iqrs/providers/homes')
-
 stop_splash(sp)
 
-
-
-rd <- rsDriver()
+#Using Selenium
+rd <- rsDriver(browser = "chrome")
 rd$client$navigate('https://dhs.iowa.gov/iqrs/providers/homes')
 h <- rd$client$getPageSource()
 h <- h[[1]] %>% read_html()
@@ -22,21 +19,14 @@ rd$client$close()
 rd$server$stop()
 rm(rd)
 
-  url <- "https://dhs.iowa.gov/iqrs/providers/homes"
+#Using rves
+url <- "https://dhs.iowa.gov/iqrs/providers/homes"
     
-  homes <- url %>%
+homes <- url %>%
       read_html() %>%  
-      html_nodes(xpath='/html/body/div[4]/div[2]/div[2]/div[2]/div/div/div/div/div[1]/div/div/div/table[2]') %>%  
       html_table()
 
+URL<-read_html("https://dhs.iowa.gov/iqrs/providers/homes")
+#find the table rows in the page
+table<-html_nodes(URL, "table")
   
-  URL<-read_html("https://dhs.iowa.gov/iqrs/providers/homes")
-  #find the table rows in the page
-  table<-html_nodes(URL, "table")
-  
-  #pull info from the table rows
-  num<-html_text(html_nodes(table, "td.u-align-right"))
-  provider<-html_text(html_nodes(table, "td.cell-provider-name"))
-  
-  #final data.frame with a table of the results
-  df<-data.frame(provider, matrix(num, ncol=3, byrow=TRUE))    
