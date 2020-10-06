dc0d56344711aa9bd5827f45c2a58c26c9b37b16
Denise Bradford <52838109+drbradford12@users.noreply.github.com>
2020-10-06 18:19:28 -0500

    Making changes to run code to the server

--- Code/json_code.R
+++ Code/json_code.R
@@ -7,12 +7,14 @@ library(RSocrata)
 library(DBI)
 library(rstudioapi)
 library(RMySQL)
+library(skimr)
 
 #TO-DO: Find the best way to download the child care data that doesn't have a JSON url
+data_sources = c()
 
 data_sources <- tibble::tribble(
   ~name, ~url,
-  "assisted living", "https://data.iowa.gov/api/odata/v4/67aj-bdft",
+   "assisted living", "https://data.iowa.gov/api/odata/v4/67aj-bdft",
   "city budget expenditures", "https://data.iowa.gov/api/odata/v4/jy6h-2e5x",
   "city budget revenue", "https://data.iowa.gov/api/odata/v4/bzed-t5zc",
   "child abuse occurrences", "https://data.iowa.gov/api/odata/v4/mh9d-fias",
@@ -28,31 +30,36 @@ data_sources <- tibble::tribble(
   "school district revenues" ,"https://data.iowa.gov/api/odata/v4/pf4i-4nww",
   "unemployment compensation fund status benefits", "https://data.iowa.gov/api/odata/v4/bbux-m3a4",
   "unemployment insurance benefit payments", "https://data.iowa.gov/api/odata/v4/aeyn-twxp",
-  "liquor_stores", "https://data.iowa.gov/api/odata/v4/ykb6-ywnd",
-  "liquor sales", "https://data.iowa.gov/api/odata/v4/m3tr-qhgy",
-  "quarterly retail sales tax", "https://data.iowa.gov/api/odata/v4/55fz-vque",
-  "registered retirement facilities", "https://data.iowa.gov/api/odata/v4/cvnj-m3t8"
+   "liquor_stores", "https://data.iowa.gov/api/odata/v4/ykb6-ywnd",
+   "liquor sales", "https://data.iowa.gov/api/odata/v4/m3tr-qhgy",
+   "quarterly retail sales tax", "https://data.iowa.gov/api/odata/v4/55fz-vque",
+   "registered retirement facilities", "https://data.iowa.gov/api/odata/v4/cvnj-m3t8"
 )
 
 #Here is the list of data sources that will take on the data directly from the JSON file
 #Export Data to MySQL Server 
 conn <- DBI::dbConnect(RMySQL::MySQL(), 
                        host = "srvanderplas.com",
-                       dbname = ,
+                       dbname = "scc",
                        user = "remote",
                        password = rstudioapi::askForPassword("Database password"))
 
-summary(conn) #We need a Dbname to get data on to the server??
-#password = "awesome-remote-mysql-server-password")
+summary(conn) 
 
 for (i in seq_along(data_sources$name)){
   data_sources <- data_sources %>% mutate(data = purrr::map(url[[i]], read.socrata))
-  #saveRDS(data_sources, file = paste0(i, ".csv"))
+  #write.csv(data_sources$data, file = paste0(data_sources$name[[i]], ".csv") , sep = ",")
+  data_name = gsub(" ", "_", data_sources$name[[i]])
   dbWriteTable(conn = conn,
-               name = paste0(i, "_mysql"),
-               value = data_sources) #Not sure if this will work just yet. We can use line 49 if it is not working
+               name = paste0(data_name, "_mysql"),
+               value = data.frame(data_sources$data),
+               row.names=FALSE,
+               nrows = 50000000,
+               #field.types=list(organization_type='BLOB'),
+               overwrite = TRUE) #Not sure if this will work just yet. We can use line 49 if it is not working
 }
 
-
-
+#Make sure that tables are added with right name and disconnet from the server ;)
+dbListTables(conn) 
+dbDisconnect(conn)
 
