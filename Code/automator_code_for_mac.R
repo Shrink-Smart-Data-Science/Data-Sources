5908a9512eec14e065a9ff9511aedfa25bd3be5d
Denise Bradford <52838109+drbradford12@users.noreply.github.com>
2020-10-25 16:42:45 -0500

    automation code on Mac

---
+++ Code/automator_code_for_mac.R
@@ -0,0 +1,18 @@
+#Scheduler for our data runs
+library(cronR)
+
+#Running things from JSON code file
+drive <- "/Users/denisebradford/Documents/Data-Sources/Code/json_code.R"
+cmd <- cron_rscript(drive)
+cron_add(command = cmd, frequency = 'minutely', 
+         id = 'update_scc_json_code', 
+         description = 'Job will update monthly with new data from JSON code')
+
+
+#Running things from html code file
+drive2 <- "/Users/denisebradford/Documents/Data-Sources/Code/html_code.R"
+cmd2 <- cron_rscript(drive2)
+cron_add(command = cmd2, frequency = 'monthly', 
+         id = 'update_scc_html_code', 
+         description = 'Job will update monthly with new data from HTML code')
+
