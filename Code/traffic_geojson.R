5104e52647e7555a41573e33979cbb2c13d7fb28
drbradford12 <denise.bradford@hudl.com>
2021-01-12 14:58:33 -0500

    Create traffic_geojson.R

---
+++ Code/traffic_geojson.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(sf)
+library(lubridate)
+library(RMySQL)
+library(DBI)
+library(keyring)
+library(geojsonio)
+library(rgdal)
+
+# --- Initial Setup ------------------------------------------------------------
+conn <- DBI::dbConnect(RMySQL::MySQL(),
+                       host = "srvanderplas.com",
+                       dbname = "scc",
+                       user = "remote",
+                       password = keyring::key_get("MY_SECRET"))
+
+dbListTables(conn) 
+
+#----Read in the GEOJSON data from the IowaDOT
+json.link <- geojson_sf("https://gis.iowadot.gov/rams/rest/services/lrs/FeatureServer/102/query?where=1%3D1&outFields=*&outSR=4326&f=json")
+  
+  
