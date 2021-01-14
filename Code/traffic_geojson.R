ab09cff3410d354c50a734c7b12481dd4b200659
Susan Vanderplas <srvanderplas@gmail.com>
2021-01-14 08:35:47 -0600

    Download the file first, then read it in. Still get warnings about malformed features but ... see if that helps for now.

--- Code/traffic_geojson.R
+++ Code/traffic_geojson.R
@@ -15,8 +15,10 @@ conn <- DBI::dbConnect(RMySQL::MySQL(),
                        password = keyring::key_get("MY_SECRET"))
 
 dbListTables(conn) 
+# --- Download GEOJSON file from Iowa DOT --------------------------------------
+json_url <- "https://gis.iowadot.gov/rams/rest/services/lrs/FeatureServer/102/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+download.file(json_url, destfile = "Data/traffic.json", mode = "wb")
 
 #----Read in the GEOJSON data from the IowaDOT
-json.link <- geojson_sf("https://gis.iowadot.gov/rams/rest/services/lrs/FeatureServer/102/query?where=1%3D1&outFields=*&outSR=4326&f=json")
-  
-  
+json.link <- geojson_sf("Data/traffic.json")
+
